a212cda31bfa9a52edddcfc03e8c3f4072c5e05b
JodieHillKitcherside <>
2023-12-18 14:09:06 +0000

    Add requirement.txt

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -3,7 +3,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
-
+else:
+     uri = os.environ.get("DATABASE_URL")
+     if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -12,3 +16,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
+
